14d5acebe0dd818ae52157909734f37a0df19882
gstv-codegeek <gichungasteve6@gmail.com>
2025-03-10 22:04:05 +0300

    implement create categories and get all categories in angular

--- ServiceHubClient/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ ServiceHubClient/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import {AdminService} from '../../services/admin.service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {NzInputDirective} from 'ng-zorro-antd/input';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {NzSpinComponent} from 'ng-zorro-antd/spin';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [
+    NzInputDirective,
+    ReactiveFormsModule,
+    NzButtonComponent,
+    NzSpinComponent
+  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
 
+  isSpinning: boolean = false;
+  categoryForm!: FormGroup;
+  categories: any = [];
+
+  constructor(private adminService: AdminService, private fb: FormBuilder, private message: NzMessageService) {
+
+  }
+
+  ngOnInit() {
+    this.categoryForm = this.fb.group({
+      categoryName: [null, [Validators.required]],
+    });
+    this.getAllCategories();
+  }
+
+  createCategory() {
+    // console.log(this.categoryForm.value)
+    this.isSpinning = true;
+    this.adminService.createCategory(this.categoryForm.value).subscribe({
+      next: (res) => {
+        this.isSpinning = false;
+        console.log(res);
+        this.message.success("Category created Successfully", {nzDuration: 5000});
+      },
+      error: (err) => {
+        this.isSpinning = false;
+        console.log(err);
+        this.message.error("Category could not be created", {nzDuration: 5000});
+      }
+    })
+  }
+
+  getAllCategories(): any {
+    this.adminService.getAllCategories().subscribe({
+      next: (res) => {
+        this.categories = res;
+        console.log(res);
+      },
+      error: (err) => {this.message.error("Something went wrong")}
+    });
+  }
 }
--- ServiceHubClient/src/app/modules/admin/services/admin.service.ts
+++ ServiceHubClient/src/app/modules/admin/services/admin.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {StorageService} from '../../../auth/services/storage/storage.service';
 
+const BASE_URL = ["http://localhost:8080/api/v1/admin"]
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {}
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      'Bearer ' + StorageService.getToken()
+    );
+  }
+
+  createCategory(categoryDto: any): Observable<any> {
+    return this.http.post(BASE_URL + "/category", categoryDto, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  getAllCategories(): Observable<any> {
+    return this.http.get(BASE_URL + "/categories", {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+
 }
