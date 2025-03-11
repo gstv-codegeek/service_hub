e53f4c5d6b059f0322958f66e639276c888950ec
gstv-codegeek <gichungasteve6@gmail.com>
2025-03-11 17:53:20 +0300

    call createService and getAllServices endpoints from angular

--- ServiceHubClient/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ ServiceHubClient/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -33,15 +33,22 @@ export class AdminDashboardComponent {
       categoryName: [null, [Validators.required]],
     });
     this.getAllCategories();
+
+    this.serviceForm = this.fb.group({
+      serviceName: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required, Validators.min(1), Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?&')]],
+      providerId: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    });
+    this.getAllServices();
   }
 
   createCategory() {
-    // console.log(this.categoryForm.value)
     this.isSpinning = true;
     this.adminService.createCategory(this.categoryForm.value).subscribe({
       next: (res) => {
         this.isSpinning = false;
-        console.log(res);
         this.message.success("Category created Successfully", {nzDuration: 5000});
       },
       error: (err) => {
@@ -53,12 +60,47 @@ export class AdminDashboardComponent {
   }
 
   getAllCategories(): any {
+    this.isSpinning = true;
     this.adminService.getAllCategories().subscribe({
       next: (res) => {
+        this.isSpinning = false;
         this.categories = res;
         console.log(res);
       },
-      error: (err) => {this.message.error("Something went wrong")}
+      error: (err) => {this.isSpinning = false; this.message.error("Something went wrong");}
     });
+  }
+
+
+  // ============= SERVICE METHODS ================= //
+  serviceForm!: FormGroup;
+  services: any = [];
+
+  createService() {
+    this.isSpinning = true;
+    this.adminService.createService(this.serviceForm.value).subscribe({
+      next: (res) => {
+        this.isSpinning=false;
+        this.message.success("Service created successfully", {nzDuration: 5000});
+      },
+      error: (err) => {
+        this.isSpinning = false;
+        this.message.error("Something went wrong");
+      }
+    });
+  }
+
+  getAllServices() {
+    this.adminService.getAllServices().subscribe({
+      next: (res) => {
+        this.isSpinning = false;
+        this.services = res;
+
+      },
+      error: (err) => {
+        this.isSpinning = false;
+        this.message.error("Something went wrong");
+      }
+    })
   }
 }
--- ServiceHubClient/src/app/modules/admin/services/admin.service.ts
+++ ServiceHubClient/src/app/modules/admin/services/admin.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {StorageService} from '../../../auth/services/storage/storage.service';
 
 const BASE_URL = ["http://localhost:8080/api/v1/admin"]
+
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   createAuthorizationHeader(): HttpHeaders {
     let authHeaders: HttpHeaders = new HttpHeaders();
@@ -34,4 +37,16 @@ export class AdminService {
   }
 
 
+  //=========== PROVIDER SERVICES ==============//
+  createService(serviceDto: any): Observable<any> {
+    return this.http.post(BASE_URL + "/service", serviceDto, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  getAllServices(): Observable<any> {
+    return this.http.get(BASE_URL + "/services", {
+      headers: this.createAuthorizationHeader()
+    })
+  }
 }
